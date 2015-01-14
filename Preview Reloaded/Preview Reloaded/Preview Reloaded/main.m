8d0593b448fb74e29499af235686266fbe52589b
Alex S <alexgitrepo@users.noreply.github.com>
2015-01-13 19:06:51 -0500

    Initial Commit

---
+++ Preview Reloaded/Preview Reloaded/Preview Reloaded/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Preview Reloaded
+//
+//  Created by Alex Scardigno on 1/13/15.
+//  Copyright (c) 2015 zzz studios. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
